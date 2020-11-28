04fc3a2d8745c31a706350411bb8645f6e189dbd
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2020-11-28 11:48:27 +0100

    Add files via upload

---
+++ messageBox/messageBox/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace messageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.textBoxRezultat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(286, 125);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(206, 30);
+            this.buttonOtvori.TabIndex = 0;
+            this.buttonOtvori.Text = "Otvorite Message Box";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxRezultat
+            // 
+            this.textBoxRezultat.Location = new System.Drawing.Point(215, 182);
+            this.textBoxRezultat.Multiline = true;
+            this.textBoxRezultat.Name = "textBoxRezultat";
+            this.textBoxRezultat.Size = new System.Drawing.Size(346, 152);
+            this.textBoxRezultat.TabIndex = 1;
+            this.textBoxRezultat.TextChanged += new System.EventHandler(this.txtBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxRezultat);
+            this.Controls.Add(this.buttonOtvori);
+            this.Name = "Form1";
+            this.Text = "Upotreba DigitalResult Enumeracije ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOtvori;
+        private System.Windows.Forms.TextBox textBoxRezultat;
+    }
+}
+
---
+++ messageBox/messageBox/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace messageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string poruka = "Odaberite Da / Ne";
+            string naslov = "Naslovna linija Dijaloga za poruke";
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme DA";
+                    break;
+
+                case DialogResult.No:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme NE";
+                    break;
+
+            }
+        }
+
+        private void txtBox_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
